24433c8123056fb9a1425cb0d179450c18ebe5fb
kali <duallovercouple@gmail.com>
2025-06-06 12:24:11 +0200

    Added operations with matrix 1 commit -matrix.h -matrix.c --Added: sum_matrices, a function that takes two not null matrices with same dimensions, same number of rows and cols, and return a matrix_float that is the sum of the 2 matrices --Added: create_identity_matrix, a function that takes a number, rank, and create a squared matrix with 1 on diagonal --Added: product_matrices, a function that takes in input 2 not null matrices, with cols of the first matrix equal to the rows of the second, and return a matrix_float that is the multiplication of the 2 matrices --Added: _min, a static local function that returns the min number between 2 --Fixed: print_matrix, when given in input NULL pointer goes to segmentation fault. RESOLUTION: added a check of non NULL pointer at the beginning of the function

--- example.c
+++ example.c
@@ -6,9 +6,16 @@
 int main(){ 
    printf(" Matrix Example.\n\r");
    
-   matrix_float* matrix = create_example_matrix( 3 );
+   matrix_float* matrix1 = create_example_matrix( 3 );
+   matrix_float* matrix2 = create_identity_matrix( 3 );
   
-  print_matrix( matrix );
+   matrix_float* matrix3 = sum_matrices( matrix1, matrix2 );
+   matrix_float* matrix4 = product_matrices( matrix1, matrix2 );
+  
+   print_matrix( matrix1 );
+   print_matrix( matrix2 );
+   print_matrix( matrix3 );
+   print_matrix( matrix4 );
   
    return 0; 
 }
--- matrix.h
+++ matrix.h
@@ -13,6 +13,10 @@ typedef struct MatrixFloatStruct{
 
 matrix_float* create_empty_float_matrix( unsigned short int rows, unsigned short int cols );
 matrix_float* create_example_matrix( unsigned short int );
+matrix_float* create_identity_matrix( unsigned short ints );
+
+matrix_float* sum_matrices( matrix_float *, matrix_float * );
+matrix_float* product_matrices( matrix_float *, matrix_float * );
 
 void print_matrix( matrix_float * );
 
